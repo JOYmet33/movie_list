5d4a9628937e3e875a28a05eea4493fdd3fee8c5
joymet33 <joymet33@gmail.com>
2024-04-26 13:59:16 +0900

    개인프로젝트(2)

--- movie.js
+++ movie.js
@@ -1,1 +1,13 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njg4YjMxYmMzZGM4NjcwNDM2ZmRhNGM5MzMzMTdiYyIsInN1YiI6IjY2MjliZTgyMGRlYTZlMDExZjc1ZGFmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JiKvL9sBXDA_vzsS1isbEXwfDgm0Fm0j7h-zxEwrYuw'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
 
